82ba76cd8de65e59688a36583b0d77a96b9adcad
Jma85 <jma24834@gmail.com>
2023-12-10 18:31:24 +0300

    adding template & static file

--- code_digest/settings.py
+++ code_digest/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["code-digest.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["code-digest.onrender.com", "127.0.0.1"]
 
 
 # Application definition
