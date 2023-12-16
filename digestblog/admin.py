bc2a0580871e06e810cd882042ad357c53060534
Jma85 <jma24834@gmail.com>
2023-12-16 13:46:33 +0300

    adding htlm, favico and comments

--- digestblog/admin.py
+++ digestblog/admin.py
@@ -1,13 +1,39 @@
-"""This file is used to register models in the admin page"""
+"""
+The Django Admin Site
+This module provides the administrative interface for Django web applications.
+It allows authorized users to create, view, edit, and delete content from the
+application's database through a user-friendly interface. The admin site can
+be customized by registering models and creating custom admin classes that
+define how each model should be displayed and edited.
+For more information, see the Django documentation:
+https://docs.djangoproject.com/en/3.2/ref/contrib/admin/
+"""
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post
+from .models import Post, Comment
 
 
-# puting my models in the admin page
+# Register your models here.
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    """This class is used to register the Post model in the admin page"""
+    """Prepopulated notes for the site"""
 
-    summernote_fields = ("content",)
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("status", "created_on")
+    list_display = ("title", "slug", "status", "created_on")
+    search_fields = ["title", "content"]
+    summernote_fields = "content"
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """User comment section model"""
+
+    list_display = ("name", "body", "post", "created_on", "approved")
+    list_filter = ("approved", "created_on")
+    search_fields = ["name", "email", "body"]
+    actions = ["approve_comments"]
+
+    def approve_comments(self, request, queryset):
+        """To approve reader comment"""
+        queryset.update(approved=True)
